d593c6f23252735daa2e404dc4959102a8500d74
Soorenje <sooren.je2006@gmail.com>
2026-08-09 00:27:01 +0330

    Fix CORS policy for Vercel domain

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://gitnest-eight.vercel.app"
+    ],
     credentials: true,
   }),
 );
